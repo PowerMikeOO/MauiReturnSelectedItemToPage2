a2e6dd20f5288ae50773022173d7fdcd1e013055
Michael Glockenmeier <michael.glockenmeier@outlook.de>
2025-04-20 19:00:10 +0200

    Add project files.

---
+++ MauiReturnSelectedItemToPage2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiReturnSelectedItemToPage2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
