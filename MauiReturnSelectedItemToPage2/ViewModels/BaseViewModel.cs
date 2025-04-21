68cd6b9d60053ea07d3289784c911d5b99a686ec
Michael Glockenmeier <michael.glockenmeier@outlook.de>
2025-04-21 12:02:01 +0200

    Add BaseViewModel for property change notifications
    Introduced a new `BaseViewModel` class in the
    `MauiReturnSelectedItemToPage2.ViewModels` namespace.
    This class implements the `INotifyPropertyChanged`
    interface, providing an event `PropertyChanged` and
    a method `OnPropertyChanged` to support data binding
    and property change notifications.

---
+++ MauiReturnSelectedItemToPage2/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MauiReturnSelectedItemToPage2.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
