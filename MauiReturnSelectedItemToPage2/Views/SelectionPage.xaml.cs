d54fe67f9ed44474668b43da1dde7295deae0c83
Michael Glockenmeier <michael.glockenmeier@outlook.de>
2025-04-21 12:20:27 +0200

    Refactor SelectionPage constructor for view model binding
    Updated the `SelectionPage` class to include a new constructor that accepts a `SelectionViewModel` parameter. Removed the default constructor and set the `BindingContext` to the provided `viewModel` for improved data binding and interaction.

--- MauiReturnSelectedItemToPage2/Views/SelectionPage.xaml.cs
+++ MauiReturnSelectedItemToPage2/Views/SelectionPage.xaml.cs
@@ -1,9 +1,13 @@
+using MauiReturnSelectedItemToPage2.ViewModels;
+
 namespace MauiReturnSelectedItemToPage2.Views;
 
 public partial class SelectionPage : ContentPage
 {
-	public SelectionPage()
-	{
-		InitializeComponent();
-	}
+    public SelectionPage(SelectionViewModel viewModel)
+    {
+        InitializeComponent();
+        // Set the navigation instance into the view model (if not already set in constructor).
+        BindingContext = viewModel;
+    }
 }
